851b2dfe247a559512ee96ab7bffe78bc3b7afd0
Sree Raj <medepalle_sreeraj@optum.com>
2022-12-09 22:49:32 +0530

    first commit

---
+++ src/components/About/index.js
@@ -0,0 +1,76 @@
+import { useEffect, useState } from 'react'
+import {
+  faJava,
+  faGitAlt,
+  faGithub,
+  faDocker,
+  faReact,
+  faJenkins,
+} from '@fortawesome/free-brands-svg-icons'
+import Loader from 'react-loaders'
+import AnimatedLetters from '../AnimatedLetters'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import './index.scss'
+
+const About = () => {
+  const [letterClass, setLetterClass] = useState('text-animate')
+
+  useEffect(() => {
+    return setTimeout(() => {
+      setLetterClass('text-animate-hover')
+    }, 3000)
+  }, [])
+
+  return (
+    <>
+      <div className="container about-page">
+        <div className="text-zone">
+          <h1>
+            <AnimatedLetters
+              letterClass={letterClass}
+              strArray={['A', 'b', 'o', 'u', 't', ' ', 'm', 'e']}
+              idx={15}
+            />
+          </h1>
+          <p>
+            A geek who is enthusiastic in exploring new technologies
+          </p>
+          <p align="LEFT">
+            I'm quiet confident, naturally curious, and perpetually working on
+            improving my chops one design problem at a time.
+          </p>
+          <p>
+            If I need to define myself in one sentence that would be a game freek :)
+             Sports fanatic, Photography enthusiast, and Tech-obsessed!!!
+          </p>
+        </div>
+
+        <div className="stage-cube-cont">
+          <div className="cubespinner">
+            <div className="face1">
+              <FontAwesomeIcon icon={faJava} color="#DD0031" />
+            </div>
+            <div className="face2">
+              <FontAwesomeIcon icon={faGithub} color="#F06529" />
+            </div>
+            <div className="face3">
+              <FontAwesomeIcon icon={faJenkins} color="#28A4D9" />
+            </div>
+            <div className="face4">
+              <FontAwesomeIcon icon={faReact} color="#5ED4F4" />
+            </div>
+            <div className="face5">
+              <FontAwesomeIcon icon={faDocker} color="#EFD81D" />
+            </div>
+            <div className="face6">
+              <FontAwesomeIcon icon={faGitAlt} color="#EC4D28" />
+            </div>
+          </div>
+        </div>
+      </div>
+      <Loader type="pacman" />
+    </>
+  )
+}
+
+export default About
